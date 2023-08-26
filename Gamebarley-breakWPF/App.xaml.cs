4bc1669907be01a531470f427876502e1af00441
Мария Сафи <safim@yandex.ru>
2023-08-26 21:19:53 +0500

    Add project files.

---
+++ Gamebarley-breakWPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Gamebarley_breakWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
